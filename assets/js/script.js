23d8b6a5e57fefb93e1d1fcd094d48ec62df450b
LykkeBergstein <1086113@ucn.dk>
2021-04-29 10:39:33 +0200

    alle the stuff TM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+//General information for sending request to WordPress 
+const apiUrl = "http://lykkebergstein.dk/wp-json/wp/v2"; 
+
+const apiUserInformation = { 
+    "username": "api.user", 
+    "password": "API-key-1234#!" 
+} ; 
 
 //Change navigation color 
 function changeTextColor() { 
@@ -15,13 +22,43 @@ function changeTextColor() {
         document.getElementById("opskrifter").style.color = "#000000"; 
     } 
 }  
-
-
-
-
 //    case "opskrifter": 
         
 
 document.getElementById("opskrifter").onclick = changeTextColor; 
 document.getElementById("om").onclick = changeTextColor; 
 document.getElementById("kontakt").onclick = changeTextColor; 
+
+//Send request to WordPress 
+const xhttp = new XMLHttpRequest() ; 
+//Specify what happens when done 
+xhttp.onreadystatechange = function() { 
+    if (this.readyState == 4 && this.status == 200) { 
+        //all fine 
+        try { 
+            let data = JSON.parse(this.response); 
+            window.localStorage.setItem('authToken', data.token); 
+            createPage(); 
+        } catch(error) { 
+            errorMessage(`Parsing error: ${error}`); 
+        } 
+    } 
+    if (this.readyState == 4 && this.status >= 400) { 
+        errorMessage("An error has occured, please try again later. "); 
+    } 
+} 
+
+xhttp.open('POST', `${apiUrl}/posts?status=private&per_page=50` , true); 
+//Specify any request headers needed 
+xhttp.setRequestHeader('Content-Type', 'application/JSON'); 
+//Send the request 
+xhttp.send(JSON.stringify(apiUserInformation)); 
+ //Omskriver JSON-objektet til string text 
+ console.log(`Yay we have a token: ${window.localStorage.getItem("authToken")} `) ; 
+/* 
+xhttp.open('GET',  , true) ; 
+//Specify any necessary headers 
+xhttp.setRequestHeader('Authorization', `Bearer ${window.localStorage.getItem('authToken')}`) ; 
+//Send request 
+xhttp.send(); 
+*/ 
